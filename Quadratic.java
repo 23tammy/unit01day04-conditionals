93f2a0d1656cb58b859f46f87cac038666686485
tammy vu <tammy.vsk@gmail.com>
2023-01-12 11:58:45 -0600

    added a == 0 conditional

--- Quadratic.java
+++ Quadratic.java
@@ -4,16 +4,22 @@ class Quadratic {
       int b = Integer.parseInt(args[1]);
       int c = Integer.parseInt(args[2]);
 
-      if (args.length < 2) {
-        System.out.println("Please provide sufficient arguements.");
-      } else if (args.length ==2) {
-        if (b*b - 4*a*c < 0) {
-          System.out.println("No real solutions.");
+      if (args.length < 3) {
+        System.out.println("provide sufficient arguements.");
+      } else {
+        if (a == 0) {
+          System.out.println("x: " + (c - b));
         } else {
-        double x1 = ((b*-1) + Math.sqrt(b*b - 4*a*c)) /(2*a);
-        double x2 = ((b*-1) - Math.sqrt(b*b - 4*a*c)) /(2*a);
-        System.out.println("x: " + x1 +","+ x2);
+          if (b*b - 4*a*c < 0) {
+            System.out.println("No real solutions.");
+          } else {
+          double x1 = ((b*-1) + Math.sqrt(b*b - 4*a*c)) /(2*a);
+          double x2 = ((b*-1) - Math.sqrt(b*b - 4*a*c)) /(2*a);
+          System.out.println("x: " + x1 +","+ x2);
+          }
+
         }
+        
       }
 
 
