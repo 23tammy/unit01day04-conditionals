6e44cf626cda2aff76c8380de4245a5265ad7834
tammy vu <tammy.vsk@gmail.com>
2023-01-12 12:24:03 -0600

    draft 1

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -1,5 +1,22 @@
 public class ThreeEqual {
     public static void main(String[] args) {
         
+        if (args.length == 3) {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = Integer.parseInt(args[2]);
+        
+            if (a == b && b == c) {
+                System.out.println("equal");
+            } else {
+                System.out.println("not equal");
+            }
+
+        } else {
+            System.out.println("provide sufficient arguments");
+        }
+        
+        
+        
     }
 }
