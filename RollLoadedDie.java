7af57d43fdac546f87e952d308a032e44ad8d178
tammy vu <tammy.vsk@gmail.com>
2023-01-13 12:21:21 -0600

    works

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -1,5 +1,11 @@
 public class RollLoadedDie {
     public static void main(String[] args) {
-        
+      
+        double roll = Math.random();
+      if (roll <= (.625)) {
+        System.out.println((Math.round(roll*6))+1);
+      } else {
+        System.out.println(6);
+      }
     }
-}
+  }
