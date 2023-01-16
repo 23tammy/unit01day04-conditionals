574ac5355812de979503391e011e3c6e82883a78
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-15 22:23:27 -0600

    RollLoadedDie.java feedback

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -1,5 +1,6 @@
 // this is pretty close to perfect. But I have OCD and am a former math teacher, so probability is really important to me:
-// 
+// I think rolling a 1, and rolling a 5, are less likely than other numbers. Because of the line with Math.round in it
+// do you agree? if so, do you know how to fix it?
 public class RollLoadedDie {
     public static void main(String[] args) {
       
